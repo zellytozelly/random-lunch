a7c4d4b9b0316cbe4c042ba551df53a65af8180d
zellytozelly <zellytozelly@gmail.com>
2022-06-05 04:02:47 +0900

    Feat: Add json interface

---
+++ src/types/foodData.d.ts
@@ -0,0 +1,11 @@
+export interface IFood {
+  id: number
+  foodImageId: number
+  foodUnit: string
+  storeId: number
+  storeName: string
+  menuName: string
+  localName: string
+  menuId: number
+  foodImageUrl: string
+}
